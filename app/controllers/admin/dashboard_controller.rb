4d2c2211202305680403400ba3afbfd9342476d6
Eszter Egyud <eszter.egyud@telus.com>
2023-01-25 18:42:39 -0700

    Add product and category information with links to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,11 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV["ADMIN_PASSWORD"]
+  # http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV["ADMIN_PASSWORD"]
+
 
   def show
+    @product_count = Product.count(:all)
+    @categories_count = Category.count(:all)
   end
+
 end
