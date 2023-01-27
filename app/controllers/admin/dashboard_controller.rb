d959db81b3475ed0e83aa7544ab13a5728e88ed1
Eszter Egyud <eszter.egyud@telus.com>
2023-01-26 23:14:14 -0700

    Implement second part of admin/sales page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,6 +6,7 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count(:all)
     @categories_count = Category.count(:all)
+    @sales_count = Sale.count(:all)
   end
 
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,20 @@
 class Sale < ApplicationRecord
+
+  # This creates an Active Record scope
+  def self.active
+    Sale.where("sales.starts_on <= ? AND sales.ends_on >= ? ", Date.current, Date.current)
+  end
+
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
+
 end
