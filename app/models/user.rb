fafe0513dec3792766599f191af8a1acabc5ce75
Eszter Egyud <eszter.egyud@telus.com>
2023-01-30 00:16:05 -0700

    Implement RSpec user_spec test cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
-class User < ApplicationRecord
-  has_secure_password 
+class User < ActiveRecord::Base
+
+  has_secure_password
+  
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 5}
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true 
+
+  def self.authenticate_with_credentials(email, password)
+    if(email)
+      user = User.find_by_email(email.strip.downcase)
+      if user && user.authenticate(password)
+        user
+      else
+        nil
+      end
+    else
+      nil
+    end
+  end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    
+  describe 'Validations' do
+
+    it "is a valid user" do
+      user = User.create(first_name: "Red", last_name: "Ridinghood", email:  "test@test.com", password: "password", password_confirmation: "password")
+      expect(user).to be_valid
+    end
+
+    it "should have a first name" do
+      user = User.create(first_name: nil, last_name: "Ridinghood", email:  "test@test.com", password: "password", password_confirmation: "password")
+      expect(user).to be_invalid
+    end
+
+    it "should have a last name" do
+      user = User.create(first_name: "Red", last_name: nil, email:  "test@test.com", password: "password", password_confirmation: "password")
+      expect(user).to be_invalid
+    end
+
+    it "should have an email" do
+      user = User.create(first_name: "Red", last_name: "Ridinghood", email:  nil, password: "password", password_confirmation: "password")
+      expect(user).to be_invalid
+    end
+
+    it "should have an password" do
+      user = User.create(first_name: "Red", last_name: "Ridinghood", email:  "test@test.com", password: nil, password_confirmation: "password")
+      expect(user).to be_invalid
+    end
+
+    it "should have an password confirmation" do
+      user = User.create(first_name: "Red", last_name: "Ridinghood", email:  "test@test.com", password: "password", password_confirmation: nil)
+      expect(user).to_not be_valid
+    end
+
+    it "should have matching passwords" do
+      user = User.create(first_name: 'Red', last_name: "Ridinghood", email:  "test@test.com", password: "password", password_confirmation: "password")
+      expect(user.password).to eq(user.password_confirmation)
+    end
+
+    it "should have a unique email" do
+      user1 = User.create(first_name: 'Red', last_name: "Ridinghood", email:  "test@test.com", password: "password", password_confirmation: "password")
+      user2 = User.create(first_name: 'Red', last_name: "Ridinghood", email:  "TEST@test.com", password: "password", password_confirmation: "password")
+      expect(user2).to_not be_valid
+    end
+
+    it "should have an password of at least 8 characters" do
+      user = User.create(first_name: "Red", last_name: "Ridinghood", email:  "test@test.com", password: "password", password_confirmation: "pass")
+      expect(user).to_not be_valid
+    end
+  end
+  
+  describe '.authenticate_with_credentials' do
+
+    it "should authenticate if email and password are valid" do
+      user = User.create(first_name: "Red", last_name: "Ridinghood", email: "test@test.com", password: "password", password_confirmation:"password")
+      authenticate = User.authenticate_with_credentials(user.email, user.password)
+      expect(authenticate).to eq(user)
+    end
+    
+    it "should have a valid email address if space is added" do
+      user = User.create(first_name: "Red", last_name: "Ridinghood", email: "test@test.com", password: "password", password_confirmation:"password")
+      authenticate = User.authenticate_with_credentials("  test@test.com ",user.password)
+      expect(authenticate).to eq(user)
+    end
+
+    it "should have a valid email address regardless of case" do
+      user = User.create(first_name: "Red", last_name: "Ridinghood", email: "test@test.com", password: "password", password_confirmation:"password")
+      authenticate = User.authenticate_with_credentials("TEST@TEST.COM", user.password)
+      expect(authenticate).to eq(user)
+    end
+  end
+end
