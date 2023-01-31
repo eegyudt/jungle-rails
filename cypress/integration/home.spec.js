daa46443052dafb4e3fed38001d7c800a23c1aca
Eszter Egyud <eszter.egyud@telus.com>
2023-01-30 18:36:14 -0700

    Implement Cypress test for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,18 @@
+describe('jungle-rails app', () => {
+  
+  describe('The Home Page', () => {
+    it('successfully loads', () => {
+      cy.visit('http://localhost:3000') // change URL to match your dev URL
+    })
+
+    it("There is products on the page", () => {
+      cy.get(".products article").should("be.visible");
+    });
+
+    it("There is 2 products on the page", () => {
+      cy.get(".products article").should("have.length", 2);
+    });
+
+  })
+ 
+})
